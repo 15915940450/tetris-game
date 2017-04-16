29fd6180a4b0102f0844a3ccd1f64c71cc574f6c
15915940450 <15915940450@139.com>
2017-04-16 20:15:56 +0800

    rotate when press up

--- js/main.js
+++ js/main.js
@@ -10,25 +10,59 @@ window.onload=function(){
     "Z":[[0,12,6,0],[2,6,4,0],[0,12,6,0],[2,6,4,0]]
   };
 
-  drawCeil('tetris_canvas',jsonAll.I,0);
+  var numWidthCanvas=300;
+  var numHeightCanvas=540;
+  var initX=0;
+
+  drawCeil('tetris_canvas',jsonAll.I,initX);
   drawCeil('upcoming_canvas',jsonAll.I,0);
-};
 
-function drawCeil(strIdCanvas,arrCeil,x){
-  var numGrid=30; //每一格的宽度
-  var eleCanvas=document.getElementById(strIdCanvas);
-  var ctx=eleCanvas.getContext('2d');
-  ctx.strokeStyle='#333';
-  ctx.fillStyle='#fee300';
-  for(var i=0;i<4;i++){
-    var ceil=arrCeil[x].slice();  //复制一个数组
+/*
+*rotate变
+*/
+  var objKeys={
+    up:38
+  };
+  document.onkeydown=function(ev){
+    // console.log(ev.keyCode);
+    ev.preventDefault();
+    switch (ev.keyCode) {
+      case objKeys.up:
+        rotate();
+        break;
+      default:
+        console.log('default');
+    }
+  };
 
-    for(var j=3;j>=0;j--){
-      if(ceil[i]>=Math.pow(2,j)){
-        ctx.fillRect(numGrid*(3-j),numGrid*i,numGrid,numGrid);
-        ctx.strokeRect(numGrid*(3-j),numGrid*i,numGrid,numGrid);
-        ceil[i]-=Math.pow(2,j);
+  function drawCeil(strIdCanvas,arrCeil,x){
+    var numGrid=30; //每一格的宽度
+    var eleCanvas=document.getElementById(strIdCanvas);
+    var ctx=eleCanvas.getContext('2d');
+
+    ctx.clearRect(0,0,numWidthCanvas,numHeightCanvas);  //清除整个画布
+    ctx.strokeStyle='#333';
+    ctx.fillStyle='#fee300';
+    ctx.translate(0.5, 0.5);  //图变清脆
+    for(var i=0;i<4;i++){
+      var ceil=arrCeil[x].slice();  //复制一个数组
+
+      for(var j=3;j>=0;j--){
+        if(ceil[i]>=Math.pow(2,j)){
+          ctx.fillRect(numGrid*(3-j),numGrid*i,numGrid,numGrid);
+          ctx.strokeRect(numGrid*(3-j),numGrid*i,numGrid,numGrid);
+          ceil[i]-=Math.pow(2,j);
+        }
       }
     }
+    ctx.translate(-0.5, -0.5);
   }
-}
+
+  function rotate(){
+    initX++;
+    if(initX>=4){
+      initX=0;
+    }
+    drawCeil('tetris_canvas',jsonAll.I,initX);
+  }
+};
